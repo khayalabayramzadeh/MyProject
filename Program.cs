6ee7dbaab8405a83d5f5c24682048e2831984801
Khayala Bayramzadeh <100376227+khayalabayramzadeh@users.noreply.github.com>
2022-03-07 21:23:26 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1stLesson
+{
+    class MyFirstProgram
+    {
+        static void Main(string[] args)
+        {
+            string name, surname, address;
+            name = "Khayala";
+            surname = "Bayramzadeh";
+            address = "Surakhani";
+            Console.WriteLine("Her name is " + name + ", " + "surname is " + surname);
+            Console.WriteLine("She lives in " + address);
+            int x, y;
+            x = 18;
+            y = 20;
+            Console.WriteLine(x - y);
+            char a = 'F';
+            bool b = false;
+            double d = 282.67;
+            float f = 356.4f;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(d * f);
+        }
+    }
+}
