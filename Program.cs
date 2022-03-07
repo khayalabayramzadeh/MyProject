595a1b66b764aba3ff14f7ecd574dbfa9a3fdbbe
Khayala Bayramzadeh <100376227+khayalabayramzadeh@users.noreply.github.com>
2022-03-07 22:18:36 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace _3rdLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, Enter your age: ");
+            int age;
+            age = int.Parse(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("You are old enough to get a driver's license.");
+
+            }
+            else
+            {
+                Console.WriteLine("You can not get a driver's license");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
