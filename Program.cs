2786f4709382905a6e792cd6517561ebf1141b49
Khayala Bayramzadeh <100376227+khayalabayramzadeh@users.noreply.github.com>
2022-03-07 22:17:15 +0400

    Add files via upload
    2ndLesson

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace SecondLesson
+{
+    class myproject2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please,Enter your name here: ");
+            string your_name = Console.ReadLine();
+            Console.WriteLine("His/Her name is " + your_name+".");
+            Console.Write("Please,Enter your surname here: ");
+            string your_surname = Console.ReadLine();
+            Console.WriteLine("His/Her surname is " + your_surname+".");
+            Console.Write("Please,Enter your age here: ");
+            string your_age = Console.ReadLine();
+            Console.WriteLine("His/Her age is " + your_age+".");
+            /*int v1 = 10;
+            int v2 = 15;
+            int v3 = 20;
+            int add, sub, div, mul, mod;
+            add = v1 + v3;
+            sub = v2 - v3;
+            div = v3 / v1;
+            mul = v1 * v2;
+            mod = v3 % v2;*/
+
+
+
+        }
+    }
+}
